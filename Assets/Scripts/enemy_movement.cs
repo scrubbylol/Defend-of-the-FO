113dfd6acd58ed4d75107b4a8e8f589fb75ee01e
Peter <zhoupeter2012@gmail.com>
2017-10-11 14:55:31 -0400

    Move enemy through path

---
+++ Assets/Scripts/enemy_movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_movement : MonoBehaviour {
+
+	public float speed = 5f;
+
+	public Vector2[] movePositions = new Vector2[5];
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+
+	void FixedUpdate () {
+		Vector2 nextPosition = movePositions [0];
+
+		transform.position = Vector2.MoveTowards (transform.position, nextPosition, speed * Time.deltaTime);
+		if (transform.position.x == nextPosition.x && transform.position.y == nextPosition.y) {
+			for (int i = 0; i < movePositions.Length-1; i++) {
+				movePositions [i] = movePositions [i + 1];
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/enemy_spwaning.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_spwaning : MonoBehaviour {
+
+	public GameObject enemy;
+	Vector2 whereToSpwan;
+	public float spwanRate = 2f;
+	float nextSpwan = 0.0f;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > nextSpwan) {
+			nextSpwan = Time.time + spwanRate;
+			whereToSpwan = gameObject.transform.position;
+			Instantiate (enemy, whereToSpwan, Quaternion.identity);
+		}
+	}
+
+}
