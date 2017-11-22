386dd4929193d0b22c8fba6fb04cfac0c6c82e0c
Randy Nguyen <nguyenr@mail.uoguelph.ca>
2017-11-22 00:22:15 -0500

    Fixed menu bugs

--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -28,11 +28,17 @@ public class menu : MonoBehaviour {
 	}
 
 	public void MapSelect() {
-		//SceneManager.LoadScene ("map1_master");
 		GameObject.Find("Main_Canvas").GetComponent<Animation>().Play("main_canvas_out");
 		GameObject.Find("Map_Canvas").GetComponent<Animation>().Play("map_canvas_in");
 		GameObject.Find ("Start").GetComponent<Button> ().enabled = true;
-		GameObject.Find ("Back").GetComponent<Button> ().enabled = true;
+		GameObject.Find ("Map_Back").GetComponent<Button> ().enabled = true;
+	}
+
+	public void HighScores() {
+		GameObject.Find("Hs_Canvas").GetComponent<Animation>().Play("hs_canvas_in");
+		GameObject.Find("Main_Canvas").GetComponent<Animation>().Play("main_canvas_out");
+		GameObject.Find ("Start").GetComponent<Button> ().enabled = true;
+		GameObject.Find ("Hs_Back").GetComponent<Button> ().enabled = true;
 	}
 
 	public void ClickMap(int map) {
@@ -57,9 +63,13 @@ public class menu : MonoBehaviour {
 		if (back == 1) {
 			GameObject.Find("Map_Canvas").GetComponent<Animation>().Play("map_canvas_out");
 			GameObject.Find("Main_Canvas").GetComponent<Animation>().Play("main_canvas_in");
+			GameObject.Find ("Start").GetComponent<Button> ().enabled = false;
+			GameObject.Find ("Map_Back").GetComponent<Button> ().enabled = false;
 		} else if (back == 2) {
 			GameObject.Find("Hs_Canvas").GetComponent<Animation>().Play("hs_canvas_out");
 			GameObject.Find("Main_Canvas").GetComponent<Animation>().Play("main_canvas_in");
+			GameObject.Find ("Start").GetComponent<Button> ().enabled = false;
+			GameObject.Find ("Hs_Back").GetComponent<Button> ().enabled = false;
 		}
 	}
 
@@ -69,11 +79,6 @@ public class menu : MonoBehaviour {
 		} else if (selectedMap == 2) {
 			SceneManager.LoadScene ("map2_master");
 		}
-	}
-
-	public void HighScores() {
-		GameObject.Find("Hs_Canvas").GetComponent<Animation>().Play("hs_canvas_in");
-		GameObject.Find("Main_Canvas").GetComponent<Animation>().Play("main_canvas_out");
 	}
 
 	public void EndGame() {
