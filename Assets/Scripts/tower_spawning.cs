2a99d1b20b3133f50de32e3c8624433d0f53d8e4
FlyingBun <patrick.yunpeng.hao@gmail.com>
2017-11-29 23:42:26 -0500

    new rock tower, damage rebalancing, new bullet prefab, various bug fixes

--- Assets/Scripts/tower_spawning.cs
+++ Assets/Scripts/tower_spawning.cs
@@ -7,6 +7,8 @@ public class tower_spawning : MonoBehaviour {
     //Tower Prefabs
     public GameObject wizard_towerPrefab;
     private GameObject wizard_tower;
+    public GameObject splash_towerPrefab;
+    private GameObject splash_tower;
     public GameObject archer_towerPrefab;
     private GameObject archer_tower;
 
@@ -16,8 +18,14 @@ public class tower_spawning : MonoBehaviour {
     //Tower Type Selection
     public Button archer_button;
     public Button wizard_button;
+    public Button splash_button;
+    public Button exit_button; 
+
     private Button archer_select;
     private Button wizard_select;
+    private Button splash_select;
+    private Button exit_select;
+
     private GameObject tower_select;
 	public AudioClip upgradeSound;
 	public AudioClip finalUpgradeSound;
@@ -33,23 +41,33 @@ public class tower_spawning : MonoBehaviour {
     }
     private bool canPlaceTower()
     {
-		return archer_tower == null && wizard_tower == null && gm.cash >= 30 && !GameObject.Find ("Options_Canvas").GetComponent<Canvas> ().enabled;
+		return archer_tower == null && splash_tower == null && wizard_tower == null && gm.cash >= 30 && !GameObject.Find ("Options_Canvas").GetComponent<Canvas> ().enabled;
+    }
+
+    private bool needUI()
+    {
+        return archer_select == null && splash_select == null && wizard_select == null && exit_select == null;
     }
     void OnMouseUp()
     {
            //2
-        if (canPlaceTower())
+        if (canPlaceTower() && needUI())
         {
             //3
 			
             archer_select = Instantiate(archer_button, new Vector2(-2,0), Quaternion.identity);
             wizard_select = Instantiate(wizard_button, new Vector2(2, 0), Quaternion.identity);
-            
+            splash_select = Instantiate(splash_button, new Vector2(0, 3), Quaternion.identity);
+            exit_select = Instantiate(exit_button, new Vector2(0, -3), Quaternion.identity);
             tower_select = GameObject.Find("Tower_Canvas");
             archer_select.transform.SetParent(tower_select.transform);
             wizard_select.transform.SetParent(tower_select.transform);
+            splash_select.transform.SetParent(tower_select.transform);
+            exit_select.transform.SetParent(tower_select.transform);
             archer_select.onClick.AddListener(placeArcherTower);
             wizard_select.onClick.AddListener(placeWizardTower);
+            splash_select.onClick.AddListener(placeSplashTower);
+            exit_select.onClick.AddListener(exitSelect);
 
         }
         else if (canUpgradeArcherTower())
@@ -59,12 +77,12 @@ public class tower_spawning : MonoBehaviour {
             if (archer_tower.GetComponent<TowerData>().levels.IndexOf(archer_tower.GetComponent<TowerData>().CurrentLevel) == 1)
             {
 				gm.playUpgradeSound ();
-				gm.SubCash(100);
+				gm.SubCash(60);
             }
             else if (archer_tower.GetComponent<TowerData>().levels.IndexOf(archer_tower.GetComponent<TowerData>().CurrentLevel) == 2)
             {
 				gm.playFinalUpgradeSound ();
-				gm.SubCash(200);
+				gm.SubCash(90);
             }
             // TODO: Deduct gold
         }
@@ -76,15 +94,51 @@ public class tower_spawning : MonoBehaviour {
             if (wizard_tower.GetComponent<TowerData>().levels.IndexOf(wizard_tower.GetComponent<TowerData>().CurrentLevel) == 1)
             {
 				gm.playUpgradeSound ();
-				gm.SubCash(120);
+				gm.SubCash(90);
             }
             else if (wizard_tower.GetComponent<TowerData>().levels.IndexOf(wizard_tower.GetComponent<TowerData>().CurrentLevel) == 2)
             {
 				gm.playFinalUpgradeSound ();
-				gm.SubCash(240);
+				gm.SubCash(150);
             }
             // TODO: Deduct gold
         }
+        else if (canUpgradeSplashTower())
+        {
+
+            splash_tower.GetComponent<TowerData>().increaseLevel();
+            Debug.Log("Leveled Up");
+            if (splash_tower.GetComponent<TowerData>().levels.IndexOf(splash_tower.GetComponent<TowerData>().CurrentLevel) == 1)
+            {
+                gm.playUpgradeSound();
+                gm.SubCash(100);
+            }
+            else if (splash_tower.GetComponent<TowerData>().levels.IndexOf(splash_tower.GetComponent<TowerData>().CurrentLevel) == 2)
+            {
+                gm.playFinalUpgradeSound();
+                gm.SubCash(170);
+            }
+            // TODO: Deduct gold
+        }
+    }
+    private void exitSelect()
+    {
+        archer_select.GetComponentInChildren<Button>().enabled = false;
+        archer_select.GetComponentInChildren<Image>().enabled = false;
+        archer_select.GetComponentInChildren<Text>().enabled = false;
+        wizard_select.GetComponentInChildren<Button>().enabled = false;
+        wizard_select.GetComponentInChildren<Image>().enabled = false;
+        wizard_select.GetComponentInChildren<Text>().enabled = false;
+        splash_select.GetComponentInChildren<Button>().enabled = false;
+        splash_select.GetComponentInChildren<Image>().enabled = false;
+        splash_select.GetComponentInChildren<Text>().enabled = false;
+        exit_select.GetComponentInChildren<Button>().enabled = false;
+        exit_select.GetComponentInChildren<Image>().enabled = false;
+        exit_select.GetComponentInChildren<Text>().enabled = false;
+        archer_select = null;
+        wizard_select = null;
+        splash_select = null;
+        exit_select = null;
     }
     private void placeWizardTower()
     {
@@ -101,8 +155,16 @@ public class tower_spawning : MonoBehaviour {
         wizard_select.GetComponentInChildren<Button>().enabled = false;
         wizard_select.GetComponentInChildren<Image>().enabled = false;
         wizard_select.GetComponentInChildren<Text>().enabled = false;
-        //Destroy(archer_select);
-        //Destroy(wizard_select);
+        splash_select.GetComponentInChildren<Button>().enabled = false;
+        splash_select.GetComponentInChildren<Image>().enabled = false;
+        splash_select.GetComponentInChildren<Text>().enabled = false;
+        exit_select.GetComponentInChildren<Button>().enabled = false;
+        exit_select.GetComponentInChildren<Image>().enabled = false;
+        exit_select.GetComponentInChildren<Text>().enabled = false;
+        archer_select = null;
+        wizard_select = null;
+        splash_select = null;
+        exit_select = null;
     }
     private void placeArcherTower()
     {
@@ -119,8 +181,47 @@ public class tower_spawning : MonoBehaviour {
         wizard_select.GetComponentInChildren<Button>().enabled = false;
         wizard_select.GetComponentInChildren<Image>().enabled = false;
         wizard_select.GetComponentInChildren<Text>().enabled = false;
+        splash_select.GetComponentInChildren<Button>().enabled = false;
+        splash_select.GetComponentInChildren<Image>().enabled = false;
+        splash_select.GetComponentInChildren<Text>().enabled = false;
+        exit_select.GetComponentInChildren<Button>().enabled = false;
+        exit_select.GetComponentInChildren<Image>().enabled = false;
+        exit_select.GetComponentInChildren<Text>().enabled = false;
+        archer_select = null;
+        wizard_select = null;
+        splash_select = null;
+        exit_select = null;
     }
-    
+
+    private void placeSplashTower()
+    {
+        gm.playConstructionSound();
+        //3
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        splash_tower = (GameObject)
+          Instantiate(splash_towerPrefab, transform.position, Quaternion.identity);
+        //4
+        gm.SubCash(30);
+        archer_select.GetComponentInChildren<Button>().enabled = false;
+        archer_select.GetComponentInChildren<Image>().enabled = false;
+        archer_select.GetComponentInChildren<Text>().enabled = false;
+        wizard_select.GetComponentInChildren<Button>().enabled = false;
+        wizard_select.GetComponentInChildren<Image>().enabled = false;
+        wizard_select.GetComponentInChildren<Text>().enabled = false;
+        splash_select.GetComponentInChildren<Button>().enabled = false;
+        splash_select.GetComponentInChildren<Image>().enabled = false;
+        splash_select.GetComponentInChildren<Text>().enabled = false;
+        exit_select.GetComponentInChildren<Button>().enabled = false;
+        exit_select.GetComponentInChildren<Image>().enabled = false;
+        exit_select.GetComponentInChildren<Text>().enabled = false;
+        archer_select = null;
+        wizard_select = null;
+        splash_select = null;
+        exit_select = null;
+    }
+
+
+
     private bool canUpgradeWizardTower()
     {
         if(wizard_tower != null)
@@ -129,11 +230,11 @@ public class tower_spawning : MonoBehaviour {
             TowerLevel nextLevel = towerData.getNextLevel();
             if (nextLevel != null)
             {
-                if (wizard_tower.GetComponent<TowerData>().levels.IndexOf(nextLevel) == 1 && gm.cash >= 100)
+                if (wizard_tower.GetComponent<TowerData>().levels.IndexOf(nextLevel) == 1 && gm.cash >= 90)
                 {
                     return true;
                 }
-                else if (wizard_tower.GetComponent<TowerData>().levels.IndexOf(nextLevel) == 2 && gm.cash >= 200)
+                else if (wizard_tower.GetComponent<TowerData>().levels.IndexOf(nextLevel) == 2 && gm.cash >= 150)
                 {
                     return true;
                 }
@@ -149,11 +250,11 @@ public class tower_spawning : MonoBehaviour {
             TowerLevel nextLevel = towerData.getNextLevel();
             if (nextLevel != null)
             {
-                if (archer_tower.GetComponent<TowerData>().levels.IndexOf(nextLevel) == 1 && gm.cash >= 100)
+                if (archer_tower.GetComponent<TowerData>().levels.IndexOf(nextLevel) == 1 && gm.cash >= 60)
                 {
                     return true;
                 }
-                else if (archer_tower.GetComponent<TowerData>().levels.IndexOf(nextLevel) == 2 && gm.cash >= 200)
+                else if (archer_tower.GetComponent<TowerData>().levels.IndexOf(nextLevel) == 2 && gm.cash >= 90)
                 {
                     return true;
                 }
@@ -162,5 +263,27 @@ public class tower_spawning : MonoBehaviour {
         
         return false;
     }
-		
+
+    private bool canUpgradeSplashTower()
+    {
+        if (splash_tower != null)
+        {
+            TowerData towerData = splash_tower.GetComponent<TowerData>();
+            TowerLevel nextLevel = towerData.getNextLevel();
+            if (nextLevel != null)
+            {
+                if (splash_tower.GetComponent<TowerData>().levels.IndexOf(nextLevel) == 1 && gm.cash >= 100)
+                {
+                    return true;
+                }
+                else if (splash_tower.GetComponent<TowerData>().levels.IndexOf(nextLevel) == 2 && gm.cash >= 170)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
 }
